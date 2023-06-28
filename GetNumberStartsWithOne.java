73944ef4f0c18c81326a5bd3005322bae9bdc44c
Parteek <parteek.sethi1@gmail.com>
2023-06-28 08:15:19 +0530

    diff way

--- GetNumberStartsWithOne.java
+++ GetNumberStartsWithOne.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GetNumberStartsWithOne {
 
@@ -12,6 +13,12 @@ public class GetNumberStartsWithOne {
 
         var collect = list.stream().map(n -> Integer.toString(n)).filter(n -> n.startsWith("1")).collect(Collectors.toList());
         System.out.println("collect = " + collect);
+
+        List<String> collect1 = list.stream().flatMap(n -> Stream.of(Integer.toString(n)))
+                                                .filter(n -> n.startsWith("1"))
+                                                .collect(Collectors.toList());
+
+        System.out.println("collect1 = " + collect1);
 
     }
 }
